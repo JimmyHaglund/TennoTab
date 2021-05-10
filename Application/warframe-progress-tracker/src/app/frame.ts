43eb679bca74c63dc5aaeb72d1f4a4e7324d089d
Jimmy <Jimmyhaglund@gmail.com>
2021-05-10 08:43:46 +0200

    Feature: Add frame properties

--- Application/warframe-progress-tracker/src/app/frame.ts
+++ Application/warframe-progress-tracker/src/app/frame.ts
@@ -1,4 +1,13 @@
 export interface Frame {
     id: number,
     name: string;
+    health: number;
+    shield: number;
+    armor: number;
+    energy: number;
+    sprintSpeed: number;
+    polarities: string;
+    exilusPolarity: string;
+    auraPolarity: string;
+    subsumedAbility: number;
 }

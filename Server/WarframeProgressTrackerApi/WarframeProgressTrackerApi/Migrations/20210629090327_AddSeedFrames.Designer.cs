02f186e267c6a4014ad0265bf28e246a21625b40
Jimmy <Jimmyhaglund@gmail.com>
2021-06-29 11:06:56 +0200

    Feature: Add frame data

--- Server/WarframeProgressTrackerApi/WarframeProgressTrackerApi/Migrations/20210629090327_AddSeedFrames.Designer.cs
+++ Server/WarframeProgressTrackerApi/WarframeProgressTrackerApi/Migrations/20210629090327_AddSeedFrames.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarframeProgressTrackerApi.Data;
 
 namespace WarframeProgressTrackerApi.Migrations
 {
     [DbContext(typeof(WarframeProgressTrackerContext))]
-    partial class WarframeProgressTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20210629090327_AddSeedFrames")]
+    partial class AddSeedFrames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Server/WarframeProgressTrackerApi/WarframeProgressTrackerApi/Models/Frame.cs
+++ Server/WarframeProgressTrackerApi/WarframeProgressTrackerApi/Models/Frame.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 namespace WarframeProgressTrackerApi.Models {
     public class Frame {
         public int Id { get; set; }
+        public int MasteryRankRequired { get; set; } = 0;
         public string Name { get; set; }
         public int Health { get; set; }
+        public float HealthMaxFactor { get; set; } = 3.0f;
         public int Shield { get; set; }
+        public float ShieldMaxFactor { get; set; } = 3.0f;
         public int Armor { get; set; }
+        public float ArmorMaxFactor { get; set; } = 1.0f;
         public int Energy { get; set; }
+        public float EnergyMaxFactor { get; set; } = 1.5f;
         public float SprintSpeed { get; set; }
         public string Polarities { get; set; }
-        public string ExilusPolarity { get; set; }
+        public string ExilusPolarity { get; set; } = "";
         public string AuraPolarity { get; set; }
         public int SubsumedAbility { get; set; }
     }
